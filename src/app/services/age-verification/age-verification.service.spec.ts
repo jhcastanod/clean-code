01fae9fd05c7676ec34b6d2b8c1efb8d018355e1
Lucas Silva <lucas.silva@contextlms.com>
2020-03-06 19:44:20 +0000

    test(service/age): fix test

--- src/app/services/age-verification/age-verification.service.spec.ts
+++ src/app/services/age-verification/age-verification.service.spec.ts
@@ -5,13 +5,12 @@ import { IUser } from 'src/app/interfaces/user';
 
 describe('AgeVerificationService', () => {
   let service: AgeVerificationService;
-  let userWithoutAge: any;
+  const userWithoutAge = { firstName: 'Lucas', lastName: 'Monstro' };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
 
     service = TestBed.get(AgeVerificationService);
-    userWithoutAge = { firstName: 'Lucas', lastName: 'Monstro' };
   });
 
   it('should be created', () => {
@@ -19,20 +18,18 @@ describe('AgeVerificationService', () => {
   });
 
   it('should contains "is legal age" when user is legal age', () => {
-    const expected = 'is legal age';
     const user: IUser = { ...userWithoutAge, age: 19 };
 
     const result = service.performAgeValidation(user);
 
-    expect(result).toContain(expected);
+    expect(result).toContain('is legal age');
   });
 
   it('should contains "is a minor" when user is minor', () => {
-    const expected = 'is a minor';
     const user: IUser = { ...userWithoutAge, age: 7 };
 
     const result = service.performAgeValidation(user);
 
-    expect(result).toContain(expected);
+    expect(result).toContain('is a minor');
   });
 });
