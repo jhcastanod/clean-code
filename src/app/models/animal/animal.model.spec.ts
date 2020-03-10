aee5c39fa16ba13a67c7918fad87fcafc6b33abf
Jorge <jcastano@seedem.co>
2020-03-10 08:20:45 -0500

    unit tests

---
+++ src/app/models/animal/animal.model.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AnimalModel } from './animal.model';
+import { AnimalCategory } from 'enums/animal-category.enum';
+
+describe('Animal Model', () => {
+  let model: AnimalModel;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AnimalModel]
+    });
+
+    model = TestBed.get(AnimalModel);
+  });
+
+  it('should return true if animal is from water', () => {
+    const water = AnimalCategory.water;
+
+    model.category = AnimalCategory.water;
+    const result = model.isFrom(water);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return false if animal is not from water', () => {
+    const air = AnimalCategory.air;
+
+    model.category = AnimalCategory.water;
+    const result = model.isFrom(air);
+
+    expect(result).toBeFalsy();
+  });
+})
