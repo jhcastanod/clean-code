40b759973f4c67d25e5bd6b444c5e203346e8b7b
Jorge <jcastano@seedem.co>
2020-03-12 12:02:23 -0500

    code review 1.5

--- src/app/providers/storage/config.provider.ts
+++ src/app/providers/storage/config.provider.ts
@@ -2,7 +2,7 @@ import { IUrlConfig } from 'interfaces/config';
 
 export const configProvider = {
   provide: 'URL_CONFIG',
-  useFactory: ({ domain, isHttps, prefixPath }: IConfig): IConfig => {
+  useFactory: ({ domain, isHttps, prefixPath }: IUrlConfig): IUrlConfig => {
     const obj = { domain, isHttps, prefixPath };
 
     return obj;
