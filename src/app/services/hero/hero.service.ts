a6b474bc34e4a803aca8bd6b2250e04c354c0507
Jorge <jcastano@seedem.co>
2020-03-13 14:16:58 -0500

    Hero Service

---
+++ src/app/services/hero/hero.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+import { HeroModel } from 'models/animal/hero/hero.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  create(hero: HeroModel) {
+  }
+}
