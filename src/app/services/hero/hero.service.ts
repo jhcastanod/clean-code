dba84eaddcbef3e6d3df63b0921ee12f33bae8ce
Jorge <jcastano@seedem.co>
2020-03-13 22:44:09 -0500

    code review 1.4

--- src/app/services/hero/hero.service.ts
+++ src/app/services/hero/hero.service.ts
@@ -6,15 +6,25 @@ import { HeroModel } from 'models/hero/hero.model';
   providedIn: 'root'
 })
 export class HeroService {
-  create(hero: HeroModel, isHurt: boolean): HeroModel {
-    const defaultValues = { fly: false, life: 100, name: 'Aldo Raine' };
-
-    if (isHurt) {
-      hero.life -= 20;
-    }
-
-    const newHero = { ...defaultValues, ...hero };
+  create(fly: boolean, life: number): HeroModel {
+    const heroBase = this.createBaseHero({ fly, life });
+    const newHero = heroBase;
 
     return newHero;
+  }
+
+  createHurtedHero(fly: boolean, life: number): HeroModel {
+    const heroBase = this.createBaseHero({ fly, life });
+    heroBase.life = 20;
+    const demageHero = heroBase;
+
+    return demageHero;
+  }
+
+  private createBaseHero(hero: HeroModel): HeroModel {
+    const defaultHeroValues = { fly: false, life: 100, name: 'Aldo Raine' };
+    const mergeDefaultAndParamValues = { ...defaultHeroValues, ...hero };
+
+    return mergeDefaultAndParamValues;
   }
 }
