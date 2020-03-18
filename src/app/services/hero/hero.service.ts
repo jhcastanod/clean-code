0d9906366f8925e4238e3133e51cb00724ba8078
Jorge <jcastano@seedem.co>
2020-03-18 00:57:05 -0500

    work in progress

--- src/app/services/hero/hero.service.ts
+++ src/app/services/hero/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 import { IHero } from '../../interfaces/hero';
 
@@ -9,13 +9,24 @@ import { IHero } from '../../interfaces/hero';
 export class HeroService {
   readonly defaultHeroValues: IHero = { fly: false, life: 100, name: 'Aldo Raine' };
   readonly endpoint = 'http://www.mocky.io/v2/5e6fb5cd33000051c3f07bb6';
+  keyOnlyErrosObj;
 
-  constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient) { }
 
   create({ fly, life, name }: IHero): IHero {
     const hero = this.createBaseHero({ fly, life, name });
-
+    const errors = this.getErrorsFromServer();
     return hero;
+  }
+
+  getErrorsFromServer() {
+    this.http.get(this.endpoint).subscribe((response) => {
+      const obj = {};
+      const errorsArr = response.errors;
+      this.keyOnlyErrosObj = errorsArr.map((e) => {
+        return; // new obj structure
+      });
+    });
   }
 
   private createBaseHero(hero: IHero): IHero {
