95eeb4be2af9df784c85c4520854ba415c44d189
Jorge <jcastano@seedem.co>
2020-03-18 21:15:54 -0500

    removing http interceptor from service structure

--- src/app/interceptors/http/bad-request.interceptor.ts
+++ src/app/interceptors/http/bad-request.interceptor.ts
@@ -1,18 +1,14 @@
-import { Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class HttpErrorHandlingServiceService implements HttpInterceptor {
+export class BadRequestHttpInterceptor implements HttpInterceptor  {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 500) {
-          // Error handling for 500
+        if (error.status === 400) {
+          // Error handling for 400
         }
         return throwError(error);
       })
