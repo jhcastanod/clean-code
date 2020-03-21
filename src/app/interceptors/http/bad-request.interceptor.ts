70064d53737c5d6327a6e16e179b1aaff13ed6d9
Jorge <jcastano@seedem.co>
2020-03-21 11:01:02 -0500

    work in progress 2.2

--- src/app/interceptors/http/bad-request.interceptor.ts
+++ src/app/interceptors/http/bad-request.interceptor.ts
@@ -1,17 +1,27 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, filter } from 'rxjs/operators';
 
-export class BadRequestHttpInterceptor implements HttpInterceptor  {
+export class BadRequestHttpInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 400) {
-          // Error handling for 400
+          this.formErrorsTransformer(error.error);
         }
         return throwError(error);
-      })
-    );
+      }
+      ));
+  }
+
+  formErrorsTransformer(error: { key: string, reason: string }[]) {
+    const obj = {};
+
+    for (const value of error) {
+      obj[value.key] = { IS_EMPTY: true };
+    }
+
+    return obj;
   }
 }
