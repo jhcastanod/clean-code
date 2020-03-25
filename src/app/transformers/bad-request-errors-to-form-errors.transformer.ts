df310c69d1f1420e10cac841d29af3e847c8063c
Lucas Silva <joao.lucas.89@hotmail.com>
2020-03-25 02:41:54 +0000

    Update bad-request-errors-to-form-errors.transformer.ts

--- src/app/transformers/bad-request-errors-to-form-errors.transformer.ts
+++ src/app/transformers/bad-request-errors-to-form-errors.transformer.ts
@@ -1,5 +1,5 @@
 export abstract class BadResquestErrorsToFormErrors {
-  static transformErrorsToFormErrors(errors: { key: string; reason: string; }[]): object {
+  static transformErrorsToFormErrors(errors: { key: string; reason: string; }[]): { [key: string]: boolean } {
     const transformedErrors = {};
 
     for (const error of errors) {
