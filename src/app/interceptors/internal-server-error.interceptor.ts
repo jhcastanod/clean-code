b0917bc104040b14a694a81d2409bfdfaa70d488
Jorge <jcastano@seedem.co>
2020-04-02 08:05:53 -0500

    code review

--- src/app/interceptors/internal-server-error.interceptor.ts
+++ src/app/interceptors/internal-server-error.interceptor.ts
@@ -2,10 +2,12 @@ import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
 import { INTERNAL_SERVER_ERROR } from 'http-status-codes';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
 import { NotificationService } from 'services/notification/notification.service';
 
 export class InternalServerErrorHttpInterceptor implements HttpInterceptor {
   constructor(private readonly notificationService: NotificationService) {}
+
   // TODO: fix type
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(catchError(this.onCatchError.bind(this)));
