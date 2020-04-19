0618d22989f6bd0e10692834802202ef0f730c48
Jorge <jcastano@seedem.co>
2020-04-19 13:42:08 -0500

    code review

--- src/app/transformers/non-words.ts
+++ src/app/transformers/non-words.ts
@@ -1,1 +1,19 @@
-export class NonWords {}
+import { TextToWords } from './text-to-words';
+
+export class NonWords extends TextToWords {
+  constructor(text: string) {
+    super(text);
+  }
+
+  hasNonWords() {
+    if (this.isTextEmpty()) {
+      return false;
+    }
+
+    if (this.extractWords().length === 0) {
+      return false;
+    }
+
+    return true;
+  }
+}
--- src/app/transformers/spanish-words.ts
+++ src/app/transformers/spanish-words.ts
@@ -1,1 +1,15 @@
-export class SpanishWords {}
+import { TextToWords } from './text-to-words';
+
+export class SpanishWords extends TextToWords {
+  hasSpanishWords() {
+    if (this.isTextEmpty()) {
+      return false;
+    }
+
+    if (this.extractWords().length === 0) {
+      return false;
+    }
+
+    return true;
+  }
+}
