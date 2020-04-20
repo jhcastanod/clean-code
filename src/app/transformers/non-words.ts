7d953e4079b75d9a1a81bb3b8326782404052e4c
Jorge <jcastano@seedem.co>
2020-04-20 16:54:52 -0500

    code review

--- src/app/transformers/non-words.ts
+++ src/app/transformers/non-words.ts
@@ -4,11 +4,11 @@ export class NonWords extends TextToWords {
   readonly nonWords = ['bitch', 'fuck', 'pussy'];
 
   hasNonWords(): boolean {
-    const words = this.extractWords();
-
     if (this.isTextEmpty()) {
       return false;
     }
+
+    const words = this.extractWords();
 
     const hasNonWords = words.some((word: string) => {
       const isNonWord = this.nonWords.includes(word);
