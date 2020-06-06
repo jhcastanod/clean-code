ee9209ee2c3f0df6dc493f436faab01440d68770
Jorge <jcastano@seedem.co>
2020-06-05 19:14:13 -0500

    code review

--- src/app/modules/design-patterns/observer/components/roulette-bet/roulette-bet.component.ts
+++ src/app/modules/design-patterns/observer/components/roulette-bet/roulette-bet.component.ts
@@ -1,25 +1,46 @@
-import { Component, OnInit, Input } from '@angular/core';
-
-import { RouletteService } from '../../services/roulette.service';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { filter } from 'rxjs/operators';
+
+import { RoulettetBetModel } from '../../models/roulette-bet.model';
+import { RouletteService } from '../../services/roulette.service';
+
 
 @Component({
   selector: 'app-roulette-bet',
   templateUrl: './roulette-bet.component.html',
   styleUrls: ['./roulette-bet.component.scss']
 })
-export class RouletteBetComponent implements OnInit {
+export class RouletteBetComponent implements OnInit, OnDestroy {
   @Input() color: string;
 
-  constructor(private readonly rouletteService: RouletteService) { }
+  betListArr: RoulettetBetModel[];
+
+  // TODO: Add type
+  rouletteSubscription$;
+
+  constructor(private readonly rouletteService: RouletteService) {}
+
+  ngOnDestroy(): void {
+    this.rouletteSubscription$.unsubscribe();
+  }
 
   ngOnInit() {
     this.betsList();
   }
 
   betsList() {
-    this.rouletteService.bets
-      .pipe(filter((bet) => bet.color === this.color))
-      .subscribe();
+    this.rouletteSubscription$ = this.rouletteService.bets
+      .pipe(filter((bet: RoulettetBetModel) => this.isSameColor(bet)))
+      .subscribe((bet: RoulettetBetModel) => this.pushBetToList(bet));
+  }
+
+  isSameColor(bet: RoulettetBetModel): boolean {
+    const sameColor = this.color === bet.color;
+
+    return sameColor;
+  }
+
+  pushBetToList(bet: RoulettetBetModel) {
+    this.betListArr.push(bet);
   }
 }
