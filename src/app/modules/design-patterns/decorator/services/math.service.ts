dee34238b15d526f2900a863ba3410b4fd7b884e
Jorge <jcastano@seedem.co>
2020-06-23 17:37:29 -0500

    code review

--- src/app/modules/design-patterns/decorator/services/math.service.ts
+++ src/app/modules/design-patterns/decorator/services/math.service.ts
@@ -5,13 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class MathService {
   fibonacci(num: number): number {
-    const isLessOrEqualThanOne = num <= 1;
-
-    if (isLessOrEqualThanOne) {
-      return 1;
-    }
-
-    const result = this.fibonacci(num - 1) + this.fibonacci(num - 2);
+    const isNumLessOrEqualOne = num <= 1;
+    const result = isNumLessOrEqualOne ? 1 : this.fibonacci(num - 1) + this.fibonacci(num - 2);
 
     return result;
   }
