5cc486c80fa277c51efc8e4a3ecef39ce3867b27
Jorge <jcastano@seedem.co>
2020-11-04 11:44:20 -0500

    code review

--- src/app/modules/design-patterns/adapter/interfaces/xml-to-json-adapter.ts
+++ src/app/modules/design-patterns/adapter/interfaces/xml-to-json-adapter.ts
@@ -1,26 +1,24 @@
-import { throwError } from 'rxjs';
-import { validateXML } from 'validate-with-xmllint';
+import { Observable, throwError } from 'rxjs';
 import { xml2json } from 'xml-js';
+
 import { IAdapter } from './adapter';
 
 export class XMLtoJsonAdapter implements IAdapter {
   constructor(private readonly xml: string) {
-    this.isXmlValid(xml);
-  }
-
-  convert() {
-    const json = xml2json(this.xml, {compact: true, spaces: 4});
-
-    return json;
-  }
-
-  isXmlValid(xml: string) {
-    const xmlNotExist = validateXML(xml);
-
-    if (xmlNotExist) {
-      return throwError('xml is invalid');
-    }
-
     this.convert();
+  }
+
+  convert(): string {
+    try {
+      const json = xml2json(this.xml, {compact: true, spaces: 4});
+
+      return json;
+    } catch (error) {
+      this.invalidXMLException(error);
+    }
+  }
+
+  invalidXMLException(error: string): Observable<string> {
+    return throwError(`xml is invalid. Error: ${error}`);
   }
 }
