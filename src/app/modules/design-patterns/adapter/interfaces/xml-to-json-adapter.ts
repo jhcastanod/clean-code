47a40d157182f623270f59ec8e0107034082e495
Jorge <jcastano@seedem.co>
2020-11-11 18:07:28 -0500

    code review

--- src/app/modules/design-patterns/adapter/interfaces/xml-to-json-adapter.ts
+++ src/app/modules/design-patterns/adapter/interfaces/xml-to-json-adapter.ts
@@ -5,6 +5,7 @@ import { IAdapter } from './adapter';
 export class XMLtoJsonAdapter implements IAdapter {
   constructor(private readonly xml: string) {}
 
+  // TODO: Add return type
   convert() {
     try {
       const json = xml2json(this.xml, {compact: true, spaces: 4});
