b5811b44961df4dc8efb6e92e79f12d22fad73a6
Jorge <jcastano@seedem.co>
2020-11-10 22:27:44 -0500

    code review

--- src/app/modules/design-patterns/adapter/interfaces/xml-to-json-adapter.ts
+++ src/app/modules/design-patterns/adapter/interfaces/xml-to-json-adapter.ts
@@ -1,24 +1,17 @@
-import { Observable, throwError } from 'rxjs';
 import { xml2json } from 'xml-js';
 
 import { IAdapter } from './adapter';
 
 export class XMLtoJsonAdapter implements IAdapter {
-  constructor(private readonly xml: string) {
-    this.convert();
-  }
+  constructor(private readonly xml: string) {}
 
-  convert(): string {
+  convert() {
     try {
       const json = xml2json(this.xml, {compact: true, spaces: 4});
 
       return json;
     } catch (error) {
-      this.invalidXMLException(error);
+      return `xml is invalid. Error: ${error}`;
     }
-  }
-
-  invalidXMLException(error: string): Observable<string> {
-    return throwError(`xml is invalid. Error: ${error}`);
   }
 }
